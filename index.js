2af8fb1c06e043e691bc402615366b80518e4747
Ibirn <kmcricciuti@gmail.com>
2020-08-27 22:32:54 +0000

    linted and tidied up.

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const { fetchMyIP } = require('./iss');
 
 fetchMyIP((error, data) => {
   if (error) {
-    console.log("It didn't work!", error)
+    console.log("It didn't work!", error);
     return;
   }
-
-  console.log("IP: ", data)
+  console.log("IP: ", data);
 });
--- iss.js
+++ iss.js
@@ -2,16 +2,21 @@ const request = require("request");
 
 const fetchMyIP = rqcallback => {
   request('https://api.ipify.org?format=json', (error, response, body) => {
-    if(error) {
-      //all end states need pass/fail
-      rqcallback("Failed to get IP." + error, null)
-      //console.log("err", error)
+    if (error) {
+      //REMINDER: all end states need both err + data passed.
+      rqcallback("Failed to get IP." + error, null);
+      return;
     }
-    rqcallback(null, body)
-    //console.log(body)
-  })
-}
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      rqcallback(Error(msg), null);
+      return;
+    }
+    rqcallback(null, JSON.parse(body).ip);
+    return;
+  });
+};
 
 module.exports = {
   fetchMyIP
-}
+};
