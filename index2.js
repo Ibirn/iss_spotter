fc8abe1d54b75b4b2105b2ca8db5708b6dbdf76e
Ibirn <kmcricciuti@gmail.com>
2020-08-28 02:20:22 +0000

    commiting completed promise version of ISS tracker.

--- index2.js
+++ index2.js
@@ -1,3 +1,7 @@
 const { localTimes } = require('./iss_promised');
 
+<<<<<<< 4d1b5db63c53c5fe55ddc191b3004901db077db2
 localTimes()
+=======
+localTimes();
+>>>>>>> commiting completed promise version of ISS tracker.
